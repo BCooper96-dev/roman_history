52ccffd4120ff58ad9387b3327b17cecd22fa4c1
BCooper96_dev <bencooper96@hotmail.co.uk>
2025-04-29 15:52:03 +0100

    paths added

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   resources :gods, only: [:index, :show]
   resources :battles, only: [:index, :show]
   resources :museums, only: [:index, :show]
+  resources :army_units, only: [:index, :show]
+  resources :roman_inventions, only: [:index, :show]
   # ... other routes ...
 end
